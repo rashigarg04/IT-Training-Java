264dc1b8a0fcab7d3f543502cef006f8eda42bff
Rashi Garg <146509998+rashigarg04@users.noreply.github.com>
2026-01-29 23:49:29 +0530

    Create ForestAnimal.java

---
+++ ForestAnimal/ForestAnimal.java
@@ -0,0 +1,15 @@
+package com.forestAnimal;
+
+public class ForestAnimal {
+    String name;
+    int age;
+
+    ForestAnimal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void makeSound() {
+        System.out.println("The animal makes a sound");
+    }
+}
